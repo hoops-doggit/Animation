4c500851fbc3dbf68be1bf819e6189d11ab99d65
Jacob <jabcog@hotmail.com>
2019-11-26 08:37:17 +1300

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/PBAnims.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PBAnims : MonoBehaviour
+{
+    private Animator animController;
+    public GameObject fx;
+    public Transform spawnPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animController = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            animController.SetBool("Walk", true);
+        }
+        else
+        {
+            animController.SetBool("Walk", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animController.SetTrigger("Jump");
+        }
+    }
+
+    public void InstantiateFX()
+    {
+        GameObject derp = Instantiate(fx,spawnPos, false);
+        derp.transform.parent = spawnPos;
+        derp.transform.localPosition = Vector3.zero;
+        derp.transform.parent = null;
+    }
+}
