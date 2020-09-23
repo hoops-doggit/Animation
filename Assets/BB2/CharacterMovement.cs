8bce9699ae18a86236c9ebe67b6834577f0d10c7
Jacob <jabcog@hotmail.com>
2020-09-23 20:06:07 +1200

    idle and friends

---
+++ Assets/BB2/CharacterMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    Vector3 movement;
+    [SerializeField] private float speed = 1;
+    [SerializeField] private Animator controller = null;
+
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.z = Input.GetAxis("Vertical");        
+
+        controller.SetFloat("BlendX", movement.x);
+        controller.SetFloat("BlendY", movement.z);
+
+        movement *= speed;
+
+
+        transform.Translate(movement);
+    }
+}
