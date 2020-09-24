ade33d64f641cd5aa1588e100c1e126a7300fe8b
jacob <jabcog@hotmail.com>
2020-09-24 13:42:36 +1200

    ParentGun

---
+++ Assets/BB2/Scripts/GunParenting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunParenting : MonoBehaviour
+{
+    [SerializeField] GameObject gun;
+    [SerializeField] Transform gunHand;
+    private Transform back;
+    Vector3 startPos;
+    Quaternion startRot;
+
+    private void Start()
+    {
+        back = gun.transform.parent;
+        startPos = gun.transform.localPosition;
+        startRot = gun.transform.localRotation;
+    }
+
+    public void ParentGun()
+    {
+        gun.transform.SetParent(gunHand);
+        Debug.Log("parented gun");
+    }
+
+    public void UnparentGun()
+    {
+        gun.transform.SetParent(back);
+        gun.transform.localPosition = startPos;
+        gun.transform.localRotation = startRot;
+    }
+
+}
